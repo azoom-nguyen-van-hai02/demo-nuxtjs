9775d275b7c5b4d5183599a3fd2ca3405399ebc7
Nguyen Van Hai <nguyen.van.hai02@azoom.jp>
2024-12-04 17:10:22 +0700

    :thumbsup: Demo add plugin vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-app>
+      <div>
+        <p>Some default layout content shared across all pages</p>
+        <app-bar />
+        <nuxt-page />
+      </div>
+
+    </v-app>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    Hello
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
